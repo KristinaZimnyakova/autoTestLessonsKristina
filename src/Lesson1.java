69b35978e086395e7b80c01995b17ba6e4cf572c
79376 <kris19932311@gmail.com>
2023-01-31 23:03:26 +0400

    2 коммит из task1

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Lesson1 {
     public void task1a() {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество рублей");
         Double amountRub = scanner.nextDouble();
@@ -10,4 +9,13 @@ public class Lesson1 {
         Double dollarExcangeRate = scanner.nextDouble();
         System.out.println(amountRub/dollarExcangeRate + " долларов");
     }
+    public void task1c() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество минут");
+        Integer inputMin = scanner.nextInt();
+        int hours = inputMin/60;
+        int min = inputMin%60;
+        System.out.println(hours + " часов " + min + " минут");
+    }
+
 }
