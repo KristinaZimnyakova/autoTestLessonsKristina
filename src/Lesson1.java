b429ad23b76b8b90147d545c3b8d66a5c55d6c09
79376 <kris19932311@gmail.com>
2023-02-01 19:01:05 +0400

    3 коммит из task1

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -7,15 +7,32 @@ public class Lesson1 {
         Double amountRub = scanner.nextDouble();
         System.out.println("Введите курс доллара");
         Double dollarExcangeRate = scanner.nextDouble();
-        System.out.println(amountRub/dollarExcangeRate + " долларов");
+        System.out.println(amountRub / dollarExcangeRate + " долларов");
     }
+
     public void task1c() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество минут");
         Integer inputMin = scanner.nextInt();
-        int hours = inputMin/60;
-        int min = inputMin%60;
+        int hours = inputMin / 60;
+        int min = inputMin % 60;
         System.out.println(hours + " часов " + min + " минут");
     }
 
+    public void task1d() {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Введите " + i + " значение");
+            int arrayValue = scanner.nextInt();
+            array[i] = arrayValue;
+        }
+        int max = array[0];
+        for (int j = 0; j < array.length; j++) {
+            if (array[j]>max) {
+                max = array[j];
+            }
+        }
+        System.out.println("Максимальное значение " + max);
+    }
 }
