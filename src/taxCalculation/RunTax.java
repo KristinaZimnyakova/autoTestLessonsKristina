b69a92c61c9930d19daebdc70b6c4c4976ddba4d
79376 <kris19932311@gmail.com>
2023-02-20 23:11:59 +0400

    2 задание черновик

--- src/taxCalculation/RunTax.java
+++ src/taxCalculation/RunTax.java
@@ -1,9 +1,16 @@
 package taxCalculation;
 
+import java.util.ArrayList;
+
 public class RunTax {
     public static void main(String[] args){
-
-
+        VAT vat = new VAT();
+        PIT pit = new PIT();
+        ArrayList<Float> tax = new ArrayList<>();
+        tax.add(vat.calculate(6000000));
+        tax.add(pit.calculate(6000000));
+        for (float i: tax) {
+        }
     }
 
 }
