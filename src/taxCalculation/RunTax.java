804c91b0059f9d90055efe7f57c327e0ec446b38
79376 <kris19932311@gmail.com>
2023-02-26 15:33:03 +0400

    2 задание c циклом по интерфейсу

--- src/taxCalculation/RunTax.java
+++ src/taxCalculation/RunTax.java
@@ -1,17 +1,18 @@
 package taxCalculation;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RunTax {
     public static void main(String[] args){
         float base = 6000000;
         ValueAddedTax valueAddedTax = new ValueAddedTax();
         ProgressiveTax progressiveTax = new ProgressiveTax();
-        ArrayList<Float> tax = new ArrayList<>();
-        tax.add(valueAddedTax.calculate(base));
-        tax.add(progressiveTax.calculate(base));
-        for (float i: tax) {
-            System.out.println(tax);
+        List<Taxable> taxableList = new ArrayList<>();
+        taxableList.add(valueAddedTax);
+        taxableList.add(progressiveTax);
+        for (Taxable i: taxableList) {
+            i.calculate(base);
         }
     }
 
